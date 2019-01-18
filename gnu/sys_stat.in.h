cd741844cb23468036357f5d80903a795456c686
KO Myung-Hun <komh@chollian.net>
2019-01-18 18:10:32 +0900

    ext: gnu: sys_stat: Fix 'implicit declaration of function' warning on OS/2 kLIBC.
    * gnu/sys_stat.in.h [kLIBC]: Include <unistd.h>.

--- gnu/sys_stat.in.h
+++ gnu/sys_stat.in.h
@@ -54,6 +54,13 @@
 
 /* The definition of _GL_WARN_ON_USE is copied here.  */
 
+/* Before doing "#define mknod rpl_mknod" below, we need to include all
+   headers that may declare mknod().  OS/2 kLIBC declares mknod() in
+   <unistd.h>, not in <sys/stat.h>.  */
+#ifdef __KLIBC__
+# include <unistd.h>
+#endif
+
 /* Before doing "#define mkdir rpl_mkdir" below, we need to include all
    headers that may declare mkdir().  Native Windows platforms declare mkdir
    in <io.h> and/or <direct.h>, not in <unistd.h>.  */
